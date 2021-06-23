b4d39c14864ba38b332fe46fe09485f022967804
yash621 <yg17381@gmail.com>
2021-06-23 10:11:32 +0530

    Header added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
-import React from "react";
+import Image from "next/image";
 
 function Header() {
   return (
-    <div>
-      <h1>hello mf</h1>
-    </div>
+    <header>
+      <div className="flex items-center bg-amazon_blue p-3 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          <Image
+            src="https://links.papareact.com/f90"
+            width={120}
+            height={40}
+            objectFit="contained"
+            className="cursor-pointer"
+          />
+          <div className="flex items-center">
+            <input type="text" />
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </header>
   );
 }
 
