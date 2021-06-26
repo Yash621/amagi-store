a185dd8d70ce2006d0346dcf67f11d8c02502ee6
yash621 <yg17381@gmail.com>
2021-06-27 03:20:03 +0530

    added stripe functionality

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
   images: {
     domains: ["www.amagi.com", "links.papareact.com", "fakestoreapi.com"],
   },
+  env: {
+    stripe_public_key: process.env.STRIKE_PUBLIC_KEY,
+  },
 };
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -4,10 +4,19 @@ import { useSelector } from "react-redux";
 import { selectItems, selectTotal } from "../slices/basketSlice";
 import CheckoutProduct from "../components/CheckoutProduct";
 import { session } from "next-auth/client";
+import { loadStripe } from "@stripe/stripe-js";
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 function Checkout() {
   const items = useSelector(selectItems);
   const totalPrice = useSelector(selectTotal);
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+    const checkoutSessions = await axios.post("/api/create-checkout-session", {
+      items: items,
+      email: session.user.email,
+    });
+  };
 
   return (
     <div className="bg-gray-100">
@@ -50,9 +59,12 @@ function Checkout() {
                 </span>
               </h2>
               <button
+                onClick={createCheckoutSession}
+                role="link"
+                disabled={!session}
                 className={`button mt-2 ${
                   !session &&
-                  "from-gray 300 to-gray-200 text-gray-300 cursor-not-allowed"
+                  "from-gray 300 to-gray-500 border-gray-200 text-gray-400 cursor-not-allowed"
                 }`}
               >
                 {!session ? "Sign in to checkout" : "Proceed to checkout"}
